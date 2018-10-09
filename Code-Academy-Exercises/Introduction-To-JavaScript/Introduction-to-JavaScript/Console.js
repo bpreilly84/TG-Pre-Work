b41a9086201906e03f121e8e114ef124e267143d
bpreilly84 <bpreilly84@gmail.com>
2018-10-09 15:15:46 -0600

    intro to java

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Console.js
@@ -0,0 +1,4 @@
+console.log(34)
+console.log(1)
+
+/* learned to use console.log */
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Properties.js
@@ -0,0 +1,3 @@
+console.log('Teaching the world how to code'.length)
+
+/* learned the .length property to log the number of characters */
