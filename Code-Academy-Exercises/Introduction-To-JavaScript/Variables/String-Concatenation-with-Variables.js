50ae60ca86a0e5fb338fea76c093abad964059c1
bpreilly84 <bpreilly84@gmail.com>
2018-10-09 16:04:20 -0600

    variables

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Concatenation-with-Variables.js
@@ -0,0 +1,4 @@
+let favoriteAnimal = 'Bird'
+console.log('My favorite animal: ' + favoriteAnimal + '.')
+
+/* used concatenation with Variables */
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,6 @@
+let myName = 'Brian';
+let myCity = 'Denver';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
+/* learned to use string interpolation */
