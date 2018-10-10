2825d9ac5626a1399ac32e873ce59bc24df33ae0
bpreilly84 <bpreilly84@gmail.com>
2018-10-09 18:47:36 -0600

    loops

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-through-Arrays.js
@@ -0,0 +1,8 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++ ){
+  console.log('I would love to visit ' + vacationSpots[i]);
+}
+
+/* learned how to loop through an array */
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,8 @@
+// Write your code below
+const vacationSpots = ['Germany', 'Japan', 'Ireland'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+/* learned about repeating loops */
