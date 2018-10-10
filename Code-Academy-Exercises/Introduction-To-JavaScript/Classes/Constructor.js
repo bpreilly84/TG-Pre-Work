e626354a5402972401308a89b11b742866c53c80
bpreilly84 <bpreilly84@gmail.com>
2018-10-09 20:20:02 -0600

    classes

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Constructor.js
@@ -0,0 +1,8 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+  }
+}
+
+/* learned constructor() method that creates a new instance of a class */
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-1.js
@@ -0,0 +1,1 @@
+/* read inheritance intro and reviewed example */
