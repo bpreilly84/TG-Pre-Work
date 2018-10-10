af77e4bf86122ea0071df136c71a91b52025f270
bpreilly84 <bpreilly84@gmail.com>
2018-10-09 21:08:52 -0600

    browser

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/npm-init.js
@@ -0,0 +1,13 @@
+{
+  "name": "learning-babel",
+  "version": "1.0.0",
+  "description": "Use Babel to transpile JavaScript ES6 to ES5",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}
+
+/* learned how to create a new file */
