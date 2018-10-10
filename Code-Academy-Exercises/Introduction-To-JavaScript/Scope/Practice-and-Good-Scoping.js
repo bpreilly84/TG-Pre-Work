2db7a623903cc66659405779023070b83b5570d5
bpreilly84 <bpreilly84@gmail.com>
2018-10-09 18:14:57 -0600

    scope

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Practice-and-Good-Scoping.js
@@ -0,0 +1,15 @@
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if (region === 'The Arctic'){
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);
+  }
+
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+/* went over other examples of how to use block scope */
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Scope.js
@@ -0,0 +1,1 @@
+/* read the inro from scope */
